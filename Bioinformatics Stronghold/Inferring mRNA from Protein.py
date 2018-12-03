8582f0ea5d9ac53aa2f80bbb2cdf15597accb541
Kaysmink <44898676+Kaysmink@users.noreply.github.com>
2018-12-03 12:14:28 +0100

    Add files via upload

---
+++ Bioinformatics Stronghold/Inferring mRNA from Protein.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 11:50:54 2018
+
+@author: kaysm
+"""
+
+Table = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+Input=open("Inferring mRNA from Protein.txt", "r") 
+Input=list(Input.read().strip())
+
+count_dict={}
+for value in Table.values():
+    if(value not in count_dict):
+        count_dict[value]=1
+    else:
+        count_dict[value]=count_dict[value]+1
+        
+possibilities=1
+for i in range(0,len(Input)):
+    possibilities=possibilities*count_dict[Input[i]]
+
+possibilities=possibilities*count_dict["STOP"]%1000000
+print(possibilities) 
+
+
+
+
+
